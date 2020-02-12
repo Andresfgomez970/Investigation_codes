999de0834b8efaa2c834ca0f1818245d1b71201b
Andresfgomez970 <andresf.gomez970@gmail.com>
2020-02-12 00:21:51 -0500

    docs: comment code, feat: make the testing for ValueInLine, fix: dependency of data generated from other program in order for this to run

--- Two_body_mappings/tests/ReadHeaderLine.c
+++ Two_body_mappings/tests/ReadHeaderLine.c
@@ -2,18 +2,27 @@
 #include <stdio.h>
 #include "../input_output.c"
 
-int main(){
-  FILE* ptr_input_file = fopen ("data/Mappings/gaussian10.dat", "r");
-  size_t total_config; 
+/* This short main test for the correct working of the functions GoTofFileLine
+  and ValueInLine */
+int main()
+{
+  char * file_name = "value_in_line10.txt";
+  char * path_name = "data/hand_made_data_for_testing/value_in_line10.txt";
+  FILE *ptr_input_file = fopen(path_name, "r");
+  size_t total_config;
+  int n = 10; // Line expected to contain the value 10
 
-  printf("Calling GoToLine(9,ptr_input_file) to read there total_config with fscanf\n"); 
-  GoToFileLine(10,ptr_input_file); 
-  fscanf(ptr_input_file,"%zu",&total_config); 
-  printf("Value obtained from gaussian10.dat: %zu\n", total_config);
+  printf("Calling GoToLine(ptr_input_file, 10) to read there an int\
+with fscanf\n");
+  GoToFileLine(ptr_input_file, 10);
+  fscanf(ptr_input_file, "%zu", &total_config);
+  printf("Value obtained from %s: %zu\n", file_name, total_config);
 
-  
-  
+  total_config = 20; 
+  ValueInLineN(ptr_input_file, "%zu", &total_config, 10);
+  printf("Value obtained from %s: %zu\n", file_name, total_config);
+
   fclose(ptr_input_file);
-  
-  return 0; 
+
+  return 0;
 }
