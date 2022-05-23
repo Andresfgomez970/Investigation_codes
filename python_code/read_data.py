b00d32f272f3db4d6479e4121f027e73149f4fdd
andresg <Andres Gomez>
2022-05-22 19:09:27 -0500

    add correct reading of functions

--- python_code/read_data.py
+++ python_code/read_data.py
@@ -2,6 +2,7 @@ from email.quoprimime import header_check
 import struct as struct
 import numpy as np
 import pandas as pd
+from regex import D
 from utils import *
 
 DOUBLE_SIZE = 8
@@ -54,6 +55,10 @@ class Header:
     def __init__(self, filename):
         self.filename = filename
         self.binary_file = self.read_file()
+        self.before_constrain_msg = True
+        self.additional_functions_read = 0
+        self.dict_of_add_functions = {1 : 'AuxF', 2 : 'ConstrainF'}
+
 
     def read_file(self):
         return open(self.filename, "rb")
@@ -76,7 +81,7 @@ class Header:
     def read_header(self):
         header, actual_line = '', 0
 
-        n_lines_header = 51 # self.lines_of_header()
+        n_lines_header = self.lines_of_header()
         # set up file from the beginning again
         self.binary_file = self.read_file()
 
@@ -97,8 +102,69 @@ class Header:
     def name_and_parameter(self, n_lines, i_line, lines):
         return lines[i_line][0] == '#' and ((i_line < n_lines - 2 and lines[i_line + 2][0] == '#') or i_line == n_lines - 2) and '-----' in lines[i_line]
     
+    def add_name_and_parameter(self, i_line, lines, header_json):
+        name = lines[i_line].replace('-', '').replace('#', '').strip()
+        header_json[name] = self.parse_value(lines[i_line + 1])
+        return header_json
+
+    def update_before_constain_message(self, i_line, lines, header_json):
+        name = lines[i_line].replace('-', '').replace('#', '').strip()
+        if name == 'ConstrainMessage':
+            self.before_constrain_msg = False
+
     def name_of_function(self, i_line, lines):
         return lines[i_line][0] == '#' and '-----' in lines[i_line]
+
+    def name_of_variable(self, i_line, lines):
+        return lines[i_line][0] == '#' and '--' in lines[i_line]
+
+    def update_mapping_functions(self, i_line, lines, header_json):
+        name = lines[i_line].replace('-', '').replace('#', '').strip()            
+        if header_json.get('mapping_functions') == None:
+            header_json['mapping_functions'] = [name]
+        else:
+            header_json['mapping_functions'].append(name)
+        return header_json
+    
+    def add_new_mapping_functions_list(self, header_json, list_name):
+        if header_json.get(list_name) == None:
+            header_json[list_name] = [[]]
+        else:
+            header_json[list_name].append([])
+        return header_json
+
+    def add_to_mapping_function_list(self, i_line, lines, header_json, list_name):
+        name = lines[i_line].replace('-', '').replace('#', '').strip()            
+        header_json[list_name][-1].append(name)
+        return header_json
+
+    def name_of_mapping_parameter(self, i_line, lines):
+        return lines[i_line][0] != '#' and '--' in lines[i_line]
+
+    def add_mapping_variable_info(self, i_line, lines, header_json):
+        header_json = self.add_to_mapping_function_list(i_line, lines, header_json, 'mapping_variable_names')
+        header_json = self.add_to_mapping_function_list(i_line + 1, lines, header_json, 'mapping_variable_val0')
+        header_json = self.add_to_mapping_function_list(i_line + 2, lines, header_json, 'mapping_variable_valf')
+        return header_json
+
+    def add_mapping_parameter_info(self, i_line, lines, header_json):
+        header_json = self.add_to_mapping_function_list(i_line, lines, header_json, 'mapping_parameters_names')
+        header_json = self.add_to_mapping_function_list(i_line + 1, lines, header_json, 'mapping_parameters_values')
+        return header_json
+
+    def add_single_name(self, i_line, lines, key, header_json):
+        name = lines[i_line].replace('-', '').replace('#', '').strip()            
+        header_json[key] = name
+        return header_json
+    
+    def read_param_and_value(self, i_line, lines, header_json, name_dict):
+        name = lines[i_line].replace('-', '').replace('#', '').strip()            
+        param_value = lines[i_line + 1].replace('-', '').replace('#', '').strip()
+        if header_json.get(name_dict) == None:
+            header_json[name_dict] = {name : param_value}
+        else:
+            header_json[name_dict][name] = param_value
+        return header_json
 
     def to_json(self):
         header_string = self.read_header()
@@ -108,74 +174,50 @@ class Header:
         i_line = 0
         while i_line < len(lines):
             if self.name_and_parameter(len(lines), i_line, lines):
-                name = lines[i_line].replace('-', '').replace('#', '').strip()
-                header_json[name] = self.parse_value(lines[i_line + 1])
-                print(lines[i_line])
-                print(lines[i_line + 1])
+                header_json = self.add_name_and_parameter(i_line, lines, header_json)
+                self.update_before_constain_message(i_line, lines, header_json)
                 i_line += 2
 
-            elif self.name_of_function(i_line, lines):
-                name = lines[i_line].replace('-', '').replace('#', '').strip()
-                print(lines[i_line])
+            elif self.name_of_function(i_line, lines) and self.before_constrain_msg:
+                header_json = self.update_mapping_functions(i_line, lines, header_json)
                 i_line += 1
-                while i_line < len(lines) and self.parse_value(lines[i_line]) != 'max_value' and not self.name_of_function(i_line, lines):
-                    print(lines[i_line])
-                    i_line += 1
+                # read until the max value of the current function
+                self.add_new_mapping_functions_list(header_json, 'mapping_variable_names')
+                
+                self.add_new_mapping_functions_list(header_json, 'mapping_variable_val0')
+                self.add_new_mapping_functions_list(header_json, 'mapping_variable_valf')
+                self.add_new_mapping_functions_list(header_json, 'mapping_functions_max_value')
+
+                self.add_new_mapping_functions_list(header_json, 'mapping_parameters_names')
+                self.add_new_mapping_functions_list(header_json, 'mapping_parameters_values')
+
+                while i_line < len(lines) and self.parse_value(lines[i_line]) != 'max_value':
+                    if self.name_of_variable(i_line, lines):
+                        self.add_mapping_variable_info(i_line, lines, header_json)
+                        i_line += 3
+
+                    elif self.name_of_mapping_parameter(i_line, lines):
+                        header_json = self.add_mapping_parameter_info(i_line, lines, header_json)
+                        i_line += 2
                 if i_line < len(lines) and self.parse_value(lines[i_line]) == 'max_value':
-                    print(lines[i_line])
-                    print(lines[i_line + 1])
+                    header_json = self.add_to_mapping_function_list(i_line + 1, lines, header_json, 'mapping_functions_max_value')
                     i_line += 2
 
-
-            else:
+            elif self.name_of_function(i_line, lines):
+                self.additional_functions_read += 1
+                func_type = self.dict_of_add_functions[self.additional_functions_read]
+                header_json = self.add_single_name(i_line, lines, func_type, header_json)
                 i_line += 1
-            # elif lines[i_line][0] == '#':
-            #     # create dictionary with name of function
-            #     name = lines[i_line].replace('-', '').replace('#', '').strip()
-            #     header_json[name] = {}
-            #     # add all its ranges and parameters
-            #     in_function_reading = True
-            #     local_actual_line = i_line
-            #     while in_function_reading:
-            #         local_actual_line += 1
-            #         if local_actual_line >= len(lines) or lines[local_actual_line] == '#': in_function_reading = False
-                    
-            #         if self.name_with_range(len(lines), local_actual_line, lines):
-            #             while local_actual_line < len(lines) or lines[local_actual_line] != '#': 
-            #                 if header_json[name] == {}:   
-            #                     param_name = lines[local_actual_line].replace('-', '').replace('#', '').strip()
-            #                     header_json[name]['param_names'] = [param_name]
-            #                     header_json[name]['val_0'] = [self.parse_value(lines[local_actual_line + 1])]
-            #                     header_json[name]['val_f'] = [self.parse_value(lines[local_actual_line + 2])]
-            #                 else:
-            #                     param_name = lines[local_actual_line].replace('-', '').replace('#', '').strip()
-            #                     header_json[name]['param_names'].append(param_name)
-            #                     header_json[name]['val_0'].append(self.parse_value(lines[local_actual_line + 1]))
-            #                     header_json[name]['val_f'].append(self.parse_value(lines[local_actual_line + 2]))
-            #                 local_actual_line += 2
-                    
-                    
-                    
-            #         elif self.param_with_value(len(lines), local_actual_line, lines):
-            #             if header_json[name] == {}:   
-            #                 param_name = lines[local_actual_line].replace('-', '').replace('#', '').strip()
-            #                 header_json[name]['param_names'] = [param_name]
-            #                 header_json[name]['values'] = [self.parse_value(lines[local_actual_line + 1])]
-            #             else:
-            #                 param_name = lines[local_actual_line].replace('-', '').replace('#', '').strip()
-            #                 header_json[name]['param_names'].append(param_name)
-            #                 header_json[name]['values'].append(self.parse_value(lines[local_actual_line + 1]))
-            #             local_actual_line += 1
-            #         else:
-            #             add_info = lines[local_actual_line].replace('-', '').replace('#', '').strip()
-            #             header_json[name][add_info] = self.parse_value(lines[local_actual_line + 1])
+                while i_line < len(lines) and not self.name_of_function(i_line, lines):
+                    header_json = self.read_param_and_value(i_line, lines, header_json, func_type + '_param')
+                    i_line += 2
 
-
-        # print(header_string)
-        # print(header_json)
+        print(header_string)
+        print(header_json)
 
 if __name__ == '__main__':
-    name = "../data/wetzel/WetzelrminFixedSimple1.dat"
+    name = "../data/defaults/DefaultLauraUniform.dat"
+    # name = "../data/wetzel/WetzelrminFixedSimple1.dat"
     print(Header(name).to_json())
 
     # print(read_header(name))
